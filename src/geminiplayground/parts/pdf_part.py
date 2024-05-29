260c8b890f17741f309c180c9a06106ca19985c0
jggomez <juan.gomez01@gmail.com>
2024-05-29 15:01:46 -0600

    feat(pdf_part):extracted images for each page and added them to the prompt

--- src/geminiplayground/parts/pdf_part.py
+++ src/geminiplayground/parts/pdf_part.py
@@ -2,17 +2,16 @@ import logging
 import typing
 import validators
 import urllib.request
-from geminiplayground.catching import cache
+from geminiplayground.parts import ImageFile
 from geminiplayground.core.gemini_client import GeminiClient
 from geminiplayground.schemas import TextPart
 from .multimodal_part import MultimodalPart
 from geminiplayground.utils import normalize_url
 from geminiplayground.utils import get_file_name_from_path
-from geminiplayground.utils import get_expire_time
 from pathlib import Path
 from urllib.error import HTTPError
-from PyPDF2 import PdfReader
 from typing import List
+import pymupdf
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -66,13 +65,16 @@ class PdfFile(MultimodalPart):
         Get the content parts for the pdf
         :return: List[TextPart]
         """
-        text_parts = []
+        parts = []
         pdf_path = get_pdf_from_anywhere(self.pdf_path)
-        with open(pdf_path, "rb") as pdf_file:
-            reader = PdfReader(pdf_file)
-            for page in reader.pages:
-                text_parts.append(TextPart(text=page.extract_text()))
-        return text_parts
+        pdf = pymupdf.open(pdf_path)
+        for page_index in range(len(pdf)):
+            page = pdf[page_index]
+            part_page = TextPart(text=(f'## Page {page_index} ##'))
+            parts.append(part_page)
+            parts.append(self._extract_text_from_page(page))
+            parts.extend(self._extract_images(pdf, page, page_index))
+        return parts
 
     def content_parts(self):
         """
@@ -80,3 +82,22 @@ class PdfFile(MultimodalPart):
         :return:
         """
         return self.__get_pdf_parts()
+
+    def _extract_images(self, pdf, page, page_index: int):
+        image_list = page.get_images()
+        images = []
+        for image_index, img in enumerate(image_list, start=1):
+            xref = img[0]
+            pix = pymupdf.Pixmap(pdf, xref)
+            if pix.n - pix.alpha > 3:
+                pix = pymupdf.Pixmap(pymupdf.csRGB, pix)
+            file_name = f"./images/page_{page_index}-image_{image_index}.png"
+            print(file_name)
+            pix.save(file_name)
+            pix = None
+            image_file = ImageFile(file_name, gemini_client=self.gemini_client)
+            images.extend(image_file.content_parts())
+        return images
+
+    def _extract_text_from_page(self, page):
+        return TextPart(text=page.get_text().encode("utf8"))

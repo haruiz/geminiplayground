541f79c723dfbb2606d32162da1874e657261525
haruiz <henryruiz22@gmail.com>
2024-08-01 21:46:21 -0500

    fix ui static files error

--- src/geminiplayground/web/ui.py
+++ src/geminiplayground/web/ui.py
@@ -1,33 +1,25 @@
 import os
-
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
-ui = FastAPI(root_path="")
-
-root_file_path = os.path.dirname(os.path.abspath(__file__))
-static_folder_root = os.path.join(root_file_path, "static")
-
-ui.mount("/", StaticFiles(directory=static_folder_root, html=True), name="static")
-
-import os
-
 from fastapi import FastAPI
 from fastapi import Request
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from starlette.responses import HTMLResponse
+from starlette.responses import HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 FILES_DIR = Path(BASE_DIR, "files")
 TEMPLATES_DIR = Jinja2Templates(directory=str(Path(BASE_DIR, "templates")))
 
-app = FastAPI()
+os.environ["FILES_DIR"] = str(FILES_DIR)
+
+ui = FastAPI()
 
 
-@app.get("/files", response_class=HTMLResponse)
+@ui.get("/files", response_class=HTMLResponse)
 def list_files(request: Request):
+    """
+    List the files in the files directory
+    """
     files = os.listdir(FILES_DIR)
     files_paths = sorted([f"{request.url._url}/{f}" for f in files])
     return TEMPLATES_DIR.TemplateResponse(
@@ -35,7 +27,15 @@ def list_files(request: Request):
     )
 
 
+@ui.get("/files/{file_name}")
+def get_file(file_name: str):
+    """
+    Get a file from the files directory
+    """
+    return FileResponse(FILES_DIR / file_name)
+
+
 root_file_path = os.path.dirname(os.path.abspath(__file__))
 static_folder_root = os.path.join(root_file_path, "static")
-app.mount("/", StaticFiles(directory=static_folder_root, html=True), name="static")
-app.mount("/files", StaticFiles(directory=FILES_DIR), name="files")
+ui.mount("/", StaticFiles(directory=static_folder_root, html=True), name="static")
+ui.mount("/files", StaticFiles(directory=FILES_DIR), name="files")
